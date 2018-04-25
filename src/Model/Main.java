b20dde4af46ce4c13ccb4a9b19cefdfa319fc62a
Kolesza Renata <kolreny@gmail.com>
2018-04-25 09:22:55 +0200

    player 2 moves

--- src/Model/Main.java
+++ src/Model/Main.java
@@ -26,7 +26,6 @@ public class Main {
 			
 			switch(s.toUpperCase()) {
 			case "D":
-				if (w1 != null)
 				w1.step(Direction.RIGHT);
 				break;
 			case "A":
@@ -38,11 +37,29 @@ public class Main {
 			case "S":
 				w1.step(Direction.DOWN);
 				break;
+			case "L":
+				w2.step(Direction.RIGHT);
+				break;
+			case "J":
+				w2.step(Direction.LEFT);
+				break;
+			case "I":
+				w2.step(Direction.UP);
+				break;
+			case "K":
+				w2.step(Direction.DOWN);
+				break;
 			case "H" :
 				w1.oilFieldWithHoney();
 				break;
 			case "O" :
 				w1.oilFieldWithOil();
+				break;
+			case "M" :
+				w2.oilFieldWithHoney();
+				break;
+			case "N" :
+				w2.oilFieldWithOil();
 				break;
 			}
 			
